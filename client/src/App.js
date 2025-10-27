e1da9217703de215ca01d4c0324d7556b432e61d
T.H.Quan <quantranhoang24@gmail.com>
2025-10-27 13:48:20 +0700

    Enviroment test

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,22 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-
-// --- ĐƯỜNG DẪN ĐÃ ĐƯỢC CẬP NHẬT ---
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import AdminLoginPage from './pages/AdminLoginPage';
-import HomePage from './pages/HomePage';
-// ---------------------------------
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
-           <h1>Hệ Thống Thuê Xe Điện (EV Rental)</h1>
-        </Link>
-        
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/admin/login" element={<AdminLoginPage />} />
-        </Routes>
-        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
